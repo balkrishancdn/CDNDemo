31e9e2f012b25f386865d565b35d358a7b4ffd0d
Balkrishan Yadav <balkrishanyadav@cdnsol.com>
2015-10-23 12:06:39 +0530

    C&P

--- GitDemoXcode/GitDemoXcode/ViewController.m
+++ GitDemoXcode/GitDemoXcode/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by balkrishanyadav on 23/10/15.
 //  Copyright © 2015 Balkrishan Yadav,CDN Software Solutions, ndore (452001). All rights reserved.
-//
+// This is testing project in cdn github account by xcode testing with git hub
 
 #import "ViewController.h"
 
@@ -16,9 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    // Do any additional setup after loading the view, typically from a nib. balkrishan Yadav
+    // test example
+    }
 
+
+-(void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
